78196b8c100cd1f8a562d0cb45efbf785726ec50
God-is-not-here22 <143888538+God-is-not-here22@users.noreply.github.com>
2024-02-24 13:45:28 +0530

    Update Delete names.py

--- Delete names.py
+++ Delete names.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('usernames.db')
 c = conn.cursor()
 
-names_to_delete = ['Parvati Jain', 'John Doe', 'Jane Doe']
+names_to_delete = ['Parvati Jain', 'John Doe', 'Jane Doe']#here
 
 for name in names_to_delete:
     c.execute("DELETE FROM usernames WHERE username = ?", (name,))
