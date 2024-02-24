9b8dc14d8441956a43911a15b5365bc832d8940e
God-is-not-here22 <143888538+God-is-not-here22@users.noreply.github.com>
2024-02-24 13:46:34 +0530

    Update Name finder.py

--- Name finder.py
+++ Name finder.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('usernames.db')
 c = conn.cursor()
 
-names_to_check = ['Echo', 'Alpha', 'Bravo']
+names_to_check = ['Echo', 'Alpha', 'Bravo']#here
 
 for name in names_to_check:
     c.execute("SELECT username FROM usernames WHERE username = ?", (name,))
